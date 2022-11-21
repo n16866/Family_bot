6a736507be8afa6704e354ecb0d570c4cf0e1289
Galina <116099759+GalinaLebedeva@users.noreply.github.com>
2022-11-21 19:06:57 +0300

    create echo bot

---
+++ main.py
@@ -0,0 +1,31 @@
+import config
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import Message
+
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.TOKEN)
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command(commands=["start"]))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+#Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
