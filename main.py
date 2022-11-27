b58918339a2d2ffadbcee9aa2c4ca2c9941aac6f
Galina <116099759+GalinaLebedeva@users.noreply.github.com>
2022-11-27 18:58:24 +0300

    add picture and make answer on message

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import FSInputFile
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -26,11 +27,25 @@ async def cmd_start(message: types.Message):
 #Создание функции, чтобы бот отвечал на "Привет" от пользователя "Доброго времени суток"
 
 @dp.message()
-async def send_text(message):
+async def router(message):
     if message.text.lower() == 'привет':
         await message.answer ('Доброго времени суток!')
     elif message.text.lower() == 'пока':
         await message.answer ('До новых встреч!')
+    elif message.text.lower() == 'солнце':
+        sun = FSInputFile("img/Sun.jpg")
+        await bot.send_photo(chat_id=message.chat.id, photo=sun)
+    elif message.text.lower() == 'луна':    
+        moon = FSInputFile("img/Moon.jpg")
+        await bot.send_photo(chat_id=message.chat.id, photo=moon)
+    elif message.text.lower() == 'звезды': 
+        stars = FSInputFile("img/Stars.jpg") 
+        await bot.send_photo(chat_id=message.chat.id, photo=stars)
+    elif message.text.lower() == 'снежинка':
+        snow = FSInputFile("img/Snowflae.jpg")
+        await bot.send_photo(chat_id=message.chat.id, photo=snow)
+    else:
+        await message.answer(message.text)
 
 
 # Запуск процесса поллинга новых апдейтов
